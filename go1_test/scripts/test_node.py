352cb8f80a63075a70aca18c635897202cf55f48
st177975 <st177975@stud.uni-stuttgart.de>
2023-07-05 13:14:02 +0200

    Updated test_node.py

--- go1_test/scripts/test_node.py
+++ go1_test/scripts/test_node.py
@@ -1,45 +1,44 @@
 #!/usr/bin/env python3
 import sys
+import numpy as np
+import cv2
+import time
 
 import rospy
 from sensor_msgs.msg import PointCloud2, Range, PointField
+import sensor_msgs.point_cloud2 as pc2
 
 range_received = False
 pointCloud_received = False
 
 def sub_callback_range(sub_msg: Range):
-    if not range_received:
-        range = sub_msg.range
+    range = sub_msg.range
 
-        rospy.loginfo(sub_msg)
-        rospy.loginfo("Current range is: ", range)
+    # rospy.loginfo(sub_msg)
+    rospy.loginfo("Current range is: ", range)
 
-        range_received = True
+    rospy.loginfo("Messages received. Node \"test_node\" will be closed.")
+    sys.exit(0)
 
 def sub_callback_PointCloud(sub_msg: PointCloud2):
-    if not pointCloud_received:
-        data = sub_msg.data
-        data_length = len(data)
-
-        rospy.loginfo(sub_msg)
-        rospy.loginfo("Length of data is: ", data_length)
-
-        pointCloud_received = True
+    cloud = pc2.read_points(sub_msg)
+    i = 0
+    timestamp = time.strftime("%H-%M-%S")
+    with open(f'PointCloud_{timestamp}.txt', 'w') as f:
+        for data in cloud:
+            f.write(str(data))
+        
 
 if __name__ == "__main__":
     # init node and log start
     rospy.init_node("test_node")
     rospy.loginfo("Node \"test_node\" started")
 
-    camera1_range_sub = rospy.Subscriber("/camera1/range_visual_face", Range, callback=sub_callback_range)
+    #camera1_range_sub = rospy.Subscriber("/camera1/range_visual_face", Range, callback=sub_callback_range)
     camera1_pointCloud_sub = rospy.Subscriber("/camera1/point_cloud_face", PointCloud2, callback=sub_callback_PointCloud)
 
     rate = rospy.Rate(2)
 
     while not rospy.is_shutdown():
-        
-        if range_received and pointCloud_received:
-            rospy.loginfo("Messages received. Node \"test_node\" will be closed.")
-            sys.exit(0)
-        
+            
         rate.sleep()
