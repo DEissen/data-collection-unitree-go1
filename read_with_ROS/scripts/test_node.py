f196e0195e609f5dfbc5320803404f980e554169
st177975 <st177975@stud.uni-stuttgart.de>
2023-07-10 09:51:58 +0200

    autoformatted test_node.py

--- read_with_ROS/scripts/test_node.py
+++ read_with_ROS/scripts/test_node.py
@@ -8,6 +8,7 @@ import rospy
 from sensor_msgs.msg import PointCloud2, Range, PointField
 import sensor_msgs.point_cloud2 as pc2
 
+
 def sub_callback_range(sub_msg: Range):
     range = sub_msg.range
 
@@ -17,7 +18,10 @@ def sub_callback_range(sub_msg: Range):
     rospy.loginfo("Messages received. Node \"test_node\" will be closed.")
     sys.exit(0)
 
+
 def sub_callback_PointCloud(sub_msg: PointCloud2):
+    pcd_file_header = "# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z\nSIZE 4 4 4\nTYPE F F F\nCOUNT 1 1 1\nWIDTH 251\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS 251\nDATA ascii"
+
     cloud = pc2.read_points(sub_msg)
     timestamp = time.strftime("%H-%M-%S")
 
@@ -37,9 +41,9 @@ def sub_callback_PointCloud(sub_msg: PointCloud2):
 
     # save array in pcd format by converting it to a numpy array first
     data_arr = np.asarray(data_arr)
-    np.savetxt(f'PointCloud_np_{timestamp}.txt', data_arr, delimiter=" ", fmt="%1.5f", comments="", header="# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z\nSIZE 4 4 4\nTYPE F F F\nCOUNT 1 1 1\nWIDTH 251\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS 251\nDATA ascii")
-   
-        
+    np.savetxt(f'PointCloud_np_{timestamp}.txt', data_arr,
+               delimiter=" ", fmt="%1.5f", comments="", header=pcd_file_header)
+
 
 if __name__ == "__main__":
     # init node and log start
@@ -47,10 +51,11 @@ if __name__ == "__main__":
     rospy.loginfo("Node \"test_node\" started")
 
     #camera1_range_sub = rospy.Subscriber("/camera1/range_visual_face", Range, callback=sub_callback_range)
-    camera1_pointCloud_sub = rospy.Subscriber("/camera1/point_cloud_face", PointCloud2, callback=sub_callback_PointCloud)
+    camera1_pointCloud_sub = rospy.Subscriber(
+        "/camera1/point_cloud_face", PointCloud2, callback=sub_callback_PointCloud)
 
     rate = rospy.Rate(5)
 
     while not rospy.is_shutdown():
-            
+
         rate.sleep()
