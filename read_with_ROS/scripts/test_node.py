d100cfb7d4fe359914adbf924b37ffc9c6ef96e3
st177975 <st177975@stud.uni-stuttgart.de>
2023-07-10 09:43:44 +0200

    Added new method to store point cloud

--- read_with_ROS/scripts/test_node.py
+++ read_with_ROS/scripts/test_node.py
@@ -20,13 +20,25 @@ def sub_callback_range(sub_msg: Range):
 def sub_callback_PointCloud(sub_msg: PointCloud2):
     cloud = pc2.read_points(sub_msg)
     timestamp = time.strftime("%H-%M-%S")
+
+    data_arr = []
+
     with open(f'PointCloud_{timestamp}.txt', 'w') as f:
         for data in cloud:
+            # method one: write directly to file
             data_to_write = str(data)
-            data_to_write.replace("(", "")
-            data_to_write.replace(")", "")
-            data_to_write.replace(" ", "")
+            # data_to_write.replace("(", "") # did not work!
+            # data_to_write.replace(")", "")
+            # data_to_write.replace(" ", "")
             f.write(data_to_write + "\n")
+
+            # method two: append to array and save it afterwards
+            data_arr.append(data)
+
+    # save array in pcd format by converting it to a numpy array first
+    data_arr = np.asarray(data_arr)
+    np.savetxt(f'PointCloud_np_{timestamp}.txt', data_arr, delimiter=" ", fmt="%1.5f", comments="", header="# .PCD v0.7 - Point Cloud Data file format\nVERSION 0.7\nFIELDS x y z\nSIZE 4 4 4\nTYPE F F F\nCOUNT 1 1 1\nWIDTH 251\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS 251\nDATA ascii")
+   
         
 
 if __name__ == "__main__":
@@ -37,7 +49,7 @@ if __name__ == "__main__":
     #camera1_range_sub = rospy.Subscriber("/camera1/range_visual_face", Range, callback=sub_callback_range)
     camera1_pointCloud_sub = rospy.Subscriber("/camera1/point_cloud_face", PointCloud2, callback=sub_callback_PointCloud)
 
-    rate = rospy.Rate(2)
+    rate = rospy.Rate(5)
 
     while not rospy.is_shutdown():
             
