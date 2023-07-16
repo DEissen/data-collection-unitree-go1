0d0d9cad4dd9c70ea1eb9c260841651ede144c1d
st177975 <st177975@stud.uni-stuttgart.de>
2023-07-16 14:40:07 +0200

    added new function which allows scp with password

--- camera/src_local_PC/copy_images.py
+++ camera/src_local_PC/copy_images.py
@@ -13,9 +13,25 @@ def copy_captured_images(ip_last_segment, destination):
     sts = os.waitpid(p.pid, 0)
     print("Copying finished successfully!")
 
+def copy_captured_images_with_sshpass(ip_last_segment, destination):
+    """
+        Function to copy measurement data from a Unitree Go1 Nano to destination.
+        NOTE: Your system must be able to ping the Nano directly (connected via LAN)!
+    """
+    target = f"unitree@192.168.123.{ip_last_segment}:/home/unitree/Unitree/UnitreecameraSDK/data"
+    
+    print(f"Start copy measurement files from Nano {ip_last_segment}")
+    p = subprocess.Popen(["sshpass", "-f", "./unitree_pwd.txt", "scp", target, destination])
+    sts = os.waitpid(p.pid, 0)
+    print("Copying finished successfully!")
+
 if __name__ == "__main__":
     destination = "/home/dominik/measurements"
 
-    copy_captured_images(13, destination)
-    copy_captured_images(14, destination)
-    copy_captured_images(15, destination)
+    # copy_captured_images(13, destination)
+    # copy_captured_images(14, destination)
+    # copy_captured_images(15, destination)
+
+    copy_captured_images_with_sshpass(13, destination)
+    copy_captured_images_with_sshpass(14, destination)
+    copy_captured_images_with_sshpass(15, destination)
