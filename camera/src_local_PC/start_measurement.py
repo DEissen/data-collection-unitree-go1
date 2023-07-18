c2e80bbe1187995590909037b1f9db8178efaf29
st177975 <st177975@stud.uni-stuttgart.de>
2023-07-18 10:19:14 +0200

    added start_camera_measurement_via_ssh() function

--- camera/src_local_PC/start_measurement.py
+++ camera/src_local_PC/start_measurement.py
@@ -10,8 +10,6 @@ def execute_command_via_ssh(ip_addr, pwd, command):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip_addr, username='unitree', password=pwd)
 
-    # path to kill processes file: unitree@192.168.123.14:/home/unitree/Documents
-
     # Execute command. The triggered process will be automatically ended in case the SSH connection is closed
     stdin, stdout, stderr = client.exec_command(command, get_pty=True)
 
@@ -21,10 +19,39 @@ def execute_command_via_ssh(ip_addr, pwd, command):
 
     client.close()
 
+def start_camera_measurement_via_ssh(ip_last_segment):
+    """
+        Function to start a SSH session to "unitree@192.168.123.{ip_last_segment}. Afterwards the camera measurement is started and it's output printed.
+        NOTE: The triggered processes will be automatically ended in case the SSH connection is closed
+    """
+    # create SSH connection
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(f"192.168.123.{ip_last_segment}", username='unitree', password="123")
+
+    # kill camera processes by executing killCameraProcesses.sh on the Nanos
+    stdin, stdout, stderr = client.exec_command("/home/unitree/Documents/killCameraProcesses.sh", get_pty=True)
+
+    # change directory to UnitreeCameraSdk
+    stdin, stdout, stderr = client.exec_command("cd /home/unitree/Unitree/sdk/UnitreeCameraSdk", get_pty=True)
+
+    if ip_last_segment == 15:
+        # Nano with IP 15 has only one camera
+        stdin, stdout, stderr = client.exec_command("./bins/getFrameOneCamera", get_pty=True) 
+    else:
+        stdin, stdout, stderr = client.exec_command("./bins/getFrameTwoCameras", get_pty=True) 
+
+    # print output of process
+    for line in stdout:
+        print(f"Update for {ip_last_segment}: {line}")
+
+    client.close()
+
+
 if __name__ == "__main__":
-    Nano13_thread = threading.Thread(target=execute_command_via_ssh, args=("192.168.123.13", '123', 'TODO'))
-    Nano14_thread = threading.Thread(target=execute_command_via_ssh, args=("192.168.123.14", '123', 'TODO'))
-    Nano15_thread = threading.Thread(target=execute_command_via_ssh, args=("192.168.123.15", '123', 'TODO'))
+    Nano13_thread = threading.Thread(target=start_camera_measurement_via_ssh, args=("13", ))
+    Nano14_thread = threading.Thread(target=start_camera_measurement_via_ssh, args=("14", ))
+    Nano15_thread = threading.Thread(target=start_camera_measurement_via_ssh, args=("15", ))
 
     Nano13_thread.start()
     Nano14_thread.start()
