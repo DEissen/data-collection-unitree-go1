7f3ca3ebc8f24f319577dd154399c8662b51a53e
st177975 <st177975@stud.uni-stuttgart.de>
2023-07-24 13:54:08 +0200

    Added new test file for image storage in case of two cameras

---
+++ camera/src_Nanos/getFrameTwoCamerasTest.cc
@@ -0,0 +1,192 @@
+#include <UnitreeCameraSDK.hpp>
+#include <unistd.h>
+
+#include <sstream>     // for stringstream
+#include <iomanip>     // for put_time
+#include <chrono>      // for timestamps
+#include <iostream>    // for std::cout
+#include <sys/stat.h>  // for mkdir
+#include <ctime>       // for std::localtime()
+#include <string>      // for sscanf, ...
+#include <thread>      // for sleep_for()
+
+auto g_base_path = "./data/";
+auto g_NameCam1 = "ChinCam";  // "ChinCam" for 13 and "RightCam" for 14
+auto g_NameCam2 = "HeadCam";  // "HeadCam" for 13 and "LeftCam" for 14
+
+std::string getMeasurementDirName(std::string base_path)
+{
+   // get timestamp for path
+   auto const now = std::chrono::system_clock::now();
+   auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+   // create pathname and return it
+   std::stringstream path;
+   path << base_path << "measurement_" << std::put_time(std::localtime(&in_time_t), "%d_%m__%H_%M");
+   return path.str();
+}
+
+std::string createMeasurementDir()
+{
+   // create path name based on timestamp for measurement
+   auto measurement_path = getMeasurementDirName(g_base_path);
+   auto cam1Path = measurement_path + "/" + g_NameCam1;
+   auto cam2Path = measurement_path + "/" + g_NameCam2;
+
+   // create measurement directory and report status
+   if (mkdir(measurement_path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1)
+   {
+      // dir alredy exists
+      if (errno == EEXIST)
+      {
+         std::cout << "Cannot create directory " << measurement_path << " as it already exists!" << std::endl;
+      }
+      // not able to create due to other issue
+      else
+      {
+         // try to crate base path which might resolve issue
+         if (mkdir(g_base_path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1)
+         {
+            // even base path creation was not successfully, thus the path is not able to be created!
+            std::cout << "Cannot create directory " << measurement_path << " due to another issue!" << std::endl;
+         }
+         else
+         {
+            // successfully created
+            std::cout << "Created base directory " << g_base_path << " first!" << std::endl;
+            // base path was created, thus try again
+            createMeasurementDir();
+         }
+      }
+   }
+   // successfully created measurement dir
+   else
+   {
+      // create dir for each camera
+      mkdir(cam1Path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+      mkdir(cam2Path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+      std::cout << "Create measurement directory (including sub dirs) " << measurement_path << " successfully!"
+                << std::endl;
+   }
+
+   return measurement_path;
+}
+
+std::string getCurrentLocalTime()
+{
+   auto now = std::chrono::system_clock::now();
+   std::time_t time = std::chrono::system_clock::to_time_t(now);
+   std::tm* tm_time = std::localtime(&time);
+   char buffer[9];
+   std::strftime(buffer, sizeof(buffer), "%H:%M:%S", tm_time);
+   return buffer;
+}
+
+int calculateTimeDifference(const std::string& target_time)
+{
+   std::string current_time = getCurrentLocalTime();
+
+   int target_hour, target_minute, target_second, current_hour, current_minute, current_second;
+   sscanf(target_time.c_str(), "%d:%d:%d", &target_hour, &target_minute, &target_second);
+   sscanf(current_time.c_str(), "%d:%d:%d", &current_hour, &current_minute, &current_second);
+
+   int target_total_seconds = target_hour * 3600 + target_minute * 60 + target_second;
+   int current_total_seconds = current_hour * 3600 + current_minute * 60 + current_second;
+
+   if (target_total_seconds <= current_total_seconds)
+   {
+      return -1;  // provided time is not in the future
+   }
+
+   return (target_total_seconds - current_total_seconds) * 1000;
+}
+
+int main(int argc, char* argv[])
+{
+   std::string measurement_path = createMeasurementDir();
+
+   int deviceNode1 = 0;  // 0 -> /dev/video0
+   int deviceNode2 = 1;  // 1 -> /dev/video1
+
+   cv::Size frameSize(1856, 800);  // defalut image size: 1856 X 800
+   int fps = 5;                    // set fps below 30 (at 30 the frame might be incomplete)
+
+   // get starting time as argument
+   if (argc < 2)
+   {
+      std::cerr << "Starting time is needed as argument in format 'HH:MM:SS'" << std::endl;
+      return 1;
+   }
+
+   UnitreeCamera cam1(deviceNode1);
+   UnitreeCamera cam2(deviceNode2);
+
+   // check whether both cams are available
+   if (!cam1.isOpened())
+      exit(EXIT_FAILURE);
+   if (!cam2.isOpened())
+      exit(EXIT_FAILURE);
+
+   // configure camera parameters
+   cam1.setRawFrameSize(frameSize);
+   cam1.setRawFrameRate(fps);
+   cam2.setRawFrameSize(frameSize);
+   cam2.setRawFrameRate(fps);
+
+   // Start camera capturing
+   cam1.startCapture();
+   cam2.startCapture();
+
+   // calculate time difference to target time
+   std::string targetTime(argv[1]);
+   int time_difference_milliseconds = calculateTimeDifference(targetTime);
+
+   if (time_difference_milliseconds < 0)
+   {
+      std::cerr << "Starting time is not in the future!" << std::endl;
+      return 1;
+   }
+
+   std::cout << "Cameras start caputring, will wait for " << (time_difference_milliseconds / 1000)
+             << "s to start with saving the images." << std::endl;
+   std::this_thread::sleep_for(std::chrono::milliseconds(time_difference_milliseconds));
+   std::cout << "Cameras start saving images now!";
+
+   while (cam1.isOpened() && cam2.isOpened())
+   {
+      // get images or wait if they are not present yet
+      cv::Mat leftCam1, rightCam1, leftCam2, rightCam2;
+      std::chrono::microseconds t;
+
+      // get time for filename
+      auto const now = std::chrono::system_clock::now();
+      auto in_time_t = std::chrono::system_clock::to_time_t(now);
+      auto transformed = now.time_since_epoch().count() / 1000000;
+      auto millis = transformed % 1000;
+      std::stringstream datetime;
+      datetime << std::put_time(std::localtime(&in_time_t), "%H_%M_%S_") << std::setw(3) << std::setfill('0')
+               << int(millis);
+
+      // store image if received
+      if (cam1.getStereoFrame(leftCam1, rightCam1, t))
+      {
+         cv::imwrite(measurement_path + "/" + g_NameCam1 + "/Right_" + datetime.str() + ".jpg", rightCam1);
+         cv::imwrite(measurement_path + "/" + g_NameCam1 + "/Left_" + datetime.str() + ".jpg", leftCam1);
+      }
+      else if (cam2.getStereoFrame(leftCam2, rightCam2, t))
+      {
+         cv::imwrite(measurement_path + "/" + g_NameCam2 + "/Right_" + datetime.str() + ".jpg", rightCam2);
+         cv::imwrite(measurement_path + "/" + g_NameCam2 + "/Left_" + datetime.str() + ".jpg", leftCam2);
+      }
+      else
+      {
+         usleep(500);
+      }
+   }
+
+   // stop camera capturing
+   cam1.stopCapture();
+   cam2.stopCapture();
+
+   return 0;
+}
