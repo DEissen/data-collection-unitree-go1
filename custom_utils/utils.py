e027be33907398453e2d9edc76a0fc68d27aff1e
st177975 <st177975@stud.uni-stuttgart.de>
2024-02-19 16:55:40 +0100

    Updated utils.py for additional floor types

--- custom_utils/utils.py
+++ custom_utils/utils.py
@@ -20,11 +20,11 @@ def get_floor_type_from_user():
         Returns:
             - floor_type (str): String with the selected floor type
     """
-    floor_types = ["invalid", "parquet", "outdoor", "vinyl flooring", "tiles"]
+    floor_types = ["invalid", "parquet", "road", "vinyl flooring", "tiles", "cobblestone", "grass"]
     not_determined_yet = True
 
     while not_determined_yet:
-        user_input = input(f"Which floor type is this measurement done for?\n1 = {floor_types[1]}\n2 = {floor_types[2]}\n3 = {floor_types[3]}\n4 = {floor_types[4]}\n")
+        user_input = input(f"Which floor type is this measurement done for?\n1 = {floor_types[1]}\n2 = {floor_types[2]}\n3 = {floor_types[3]}\n4 = {floor_types[4]}\nn5 = {floor_types[5]}\nn6 = {floor_types[6]}\n")
 
         # if conversion to int is not working it's an invalid input (can be determined by setting it to 0)
         try:
@@ -32,7 +32,7 @@ def get_floor_type_from_user():
         except:
             user_input = 0
 
-        if user_input == 1 or user_input == 2 or user_input == 3 or user_input == 4:
+        if user_input == 1 or user_input == 2 or user_input == 3 or user_input == 4 or user_input == 5 or user_input == 6:
             not_determined_yet = False
         else:
             print("\nInvalid input, please try again!\n")
